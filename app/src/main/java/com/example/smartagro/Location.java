6dc7906b4f4367225659c106df9b80fb6db20cb0
Jubair <4logichunter@gmail.com>
2019-10-31 17:32:38 +0600

    First

---
+++ app/src/main/java/com/example/smartagro/Location.java
@@ -0,0 +1,42 @@
+package com.example.smartagro;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatDialogFragment;
+
+public class Location extends AppCompatDialogFragment {
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder= new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.activity_bottom_sheet, null);
+
+        builder.setView(view)
+                .setTitle("Login")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                       // String username = edit_username.getText().toString();
+                       // String password = editTextPassword.getText().toString();
+
+                    }
+                });
+
+
+        return builder.create();
+    }
+}
--- app/src/main/java/com/example/smartagro/RegisterActivity.java
+++ app/src/main/java/com/example/smartagro/RegisterActivity.java
@@ -2,13 +2,18 @@ package com.example.smartagro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class RegisterActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+
     }
 }
