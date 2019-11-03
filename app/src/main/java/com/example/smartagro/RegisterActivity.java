7e33412160e2987862aa47995649bd678f0a4ac1
4logichunter <4logichunter@gmail.com>
2019-11-03 16:05:17 +0600

    oo

--- app/src/main/java/com/example/smartagro/RegisterActivity.java
+++ app/src/main/java/com/example/smartagro/RegisterActivity.java
@@ -17,14 +17,7 @@ public class RegisterActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-        Spinner spinner = (Spinner) findViewById(R.id.planets_spinner);
-// Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.planets_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
-        spinner.setAdapter(adapter);
+      
 
         imageButton =  findViewById(R.id.btnLocation);
         imageButton.setOnClickListener(new View.OnClickListener() {
