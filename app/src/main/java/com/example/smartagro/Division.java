220846eb70bee83de0963f848134cd656f6188f7
Jubair <4logichunter@gmail.com>
2019-11-04 11:08:04 +0600

    GEOLocation

--- app/src/main/java/com/example/smartagro/Division.java
+++ app/src/main/java/com/example/smartagro/Division.java
@@ -2,34 +2,105 @@ package com.example.smartagro;
 
 
 import com.google.gson.annotations.SerializedName;
-
 public class Division {
 
-    @SerializedName("divisionCode")
     private int divisionCode;
+
+    @SerializedName("divisionName")
+
+    private String divisionName;
+
+    public int getDivisionCode() {
+        return divisionCode;
+    }
+
+    public String getDivisionName() {
+        return divisionName;
+    }
+}
+
+
+
+//public class Division {
+//
+//
+//    private int userId;
+//
+//    private int id;
+//
+//    private String title;
+//
+//    @SerializedName("body")
+//    private String text;
+//
+//    public Division(int userId, int id, String title, String text) {
+//        this.userId = userId;
+//        this.id = id;
+//        this.title = title;
+//        this.text = text;
+//    }
+//
+//    public void setUserId(int userId) {
+//        this.userId = userId;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public void setText(String text) {
+//        this.text = text;
+//    }
+//
+//    public int getUserId() {
+//        return userId;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public String getText() {
+//        return text;
+//    }
+
+    /* @SerializedName("divisionCode")
+    @Expose
+    private int divisionCode;
+
+    @SerializedName("divisionName")
+    @Expose
+    private String divisionName;
+
+    public Division() {
+    }
 
     public Division(int divisionCode, String divisionName) {
         this.divisionCode = divisionCode;
         this.divisionName = divisionName;
     }
 
-    public int getDivisionCode() {
+    public int getdivisionCode() {
         return divisionCode;
     }
 
-    public void setDivisionCode(int divisionCode) {
+    public void setdivisionCode(int divisionCode) {
         this.divisionCode = divisionCode;
     }
 
-    public String getDivisionName() {
+    public String getdivisionName() {
         return divisionName;
     }
 
-    public void setDivisionName(String divisionName) {
-        this.divisionName = divisionName;
-    }
-
-    @SerializedName("divisionName")
-    private String divisionName;
-
-}
+    public void setdivisionName(String name) {
+        this.divisionName = name;
+    }*/
+//}
--- app/src/main/java/com/example/smartagro/FarmerLocationActivity.java
+++ app/src/main/java/com/example/smartagro/FarmerLocationActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 
 import android.widget.Spinner;
@@ -24,7 +25,7 @@ import retrofit2.Response;
 
 
 public class FarmerLocationActivity extends AppCompatActivity {
-    private List<Division> posts;
+    private static  List<Division> posts;
     Button btnGetDivisionList;
     private Spinner spinner;
 
@@ -41,37 +42,48 @@ public class FarmerLocationActivity extends AppCompatActivity {
 
         btnGetDivisionList = findViewById(R.id.btnGetDivison);
 
+        getUsersList();
 
-        btnGetDivisionList.setOnClickListener(new View.OnClickListener() {
+       /* Spinner s = (Spinner) findViewById(R.id.spinnerDivison);
+        ArrayAdapter<Division> adapter = new ArrayAdapter<Division>(this,
+                android.R.layout.simple_spinner_item, posts);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        s.setAdapter(adapter);*/
+
+
+        /*btnGetDivisionList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                getDivisonList();
+                getUsersList();
             }
-        });
+        });*/
 
     }
 
-    public void getDivisonList() {
+    public void getUsersList() {
         Call<List<Division>> call = RetrofitClient
                 .getInstance()
                 .getApi().getDivision();
-
-
-
+        // Call<List<Division>> call = userService.getDivision();
         call.enqueue(new Callback<List<Division>>() {
             @Override
             public void onResponse(Call<List<Division>> call, Response<List<Division>> response) {
 
-               // Toast.makeText(getApplication().getApplicationContext(),"sdfsdf",Toast.LENGTH_LONG);
-
-boolean mo=response.isSuccessful();
-                String asd="sfdsf";
-
                 if (response.isSuccessful()) {
                     list = response.body();
 
+                    posts = response.body();
 
+                    /*for (Division post : posts) {
+                        String content = "";
+                        content += "ID: " + post.getId() + "\n";
+                        content += "User ID: " + post.getUserId() + "\n";
+                        content += "Title: " + post.getTitle() + "\n";
+                        content += "Text: " + post.getText() + "\n\n";
+
+                        //textViewResult.append(content);
+                    }*/
                 }
             }
 
